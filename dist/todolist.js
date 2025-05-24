cb50db871bb4cb08864066462cf68e94bcae0ad1
Reza <126569202+rezaghazikhani@users.noreply.github.com>
2025-05-24 13:34:34 +0330

    add funcion remove todo

--- dist/todolist.js
+++ dist/todolist.js
@@ -1,26 +1,40 @@
 const todoValue = document.querySelector(".todo-value");
 const addTodo = document.querySelector(".add-todo");
-const clearAll = document.querySelector(".clear-todos");
+const clearTodos = document.querySelector(".clear-todos");
 const todoList = document.querySelector(".todoList");
-const nandleSubmit = (event) => {
+let todos = JSON.parse(localStorage.getItem("todos") || "[]");
+const handleSubmit = (event) => {
     event.preventDefault();
     const newTodo = {
         id: crypto.randomUUID(),
         title: todoValue.value,
-        isComplete: false
+        isComplete: false,
     };
     addTodoToDom(newTodo);
+    todos.push(newTodo);
+    saveTodosInLocalStorage();
     todoValue.value = "";
     todoValue.focus();
 };
 const addTodoToDom = (todo) => {
-    todoList.insertAdjacentHTML('beforeend', `
-        <li>
-        ${todo.title}<span class="icon"">
-          <i class="fas fa-trash"></i>
-          </span>
+    todoList.insertAdjacentHTML("beforeend", `
+        <li onclick="removeTodo('${todo.id}')">
+          ${todo.title}<span class="icon"
+            ><i class="fas fa-trash"></i
+          ></span>
         </li>
     `);
 };
-addTodo.addEventListener('click', (event) => nandleSubmit(event));
+const saveTodosInLocalStorage = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+    return true;
+};
+const removeTodo = (todoID) => {
+    todos = todos.filter((todo) => todo.id !== todoID);
+    saveTodosInLocalStorage();
+    todoList.innerHTML = "";
+    todos.forEach((todo) => addTodoToDom(todo));
+};
+addTodo.addEventListener("click", (event) => handleSubmit(event));
+window.addEventListener("DOMContentLoaded", () => todos.forEach((todo) => addTodoToDom(todo)));
 //# sourceMappingURL=todolist.js.map
--- src/todolist.ts
+++ src/todolist.ts
@@ -1,36 +1,60 @@
 const todoValue = document.querySelector(".todo-value") as HTMLInputElement;
 const addTodo = document.querySelector(".add-todo") as HTMLButtonElement;
-const clearAll = document.querySelector(".clear-todos") as HTMLButtonElement;
+const clearTodos = document.querySelector(".clear-todos") as HTMLButtonElement;
 const todoList = document.querySelector(".todoList") as HTMLUListElement;
 
-
 interface Todo {
-    id: string,
-    title: string,
-    isComplete: boolean
+  id: string;
+  title: string;
+  isComplete: boolean;
 }
 
-const nandleSubmit = (event: Event) => {
-    event.preventDefault();
-    const newTodo: Todo = {
-        id: crypto.randomUUID(),
-        title: todoValue.value,
-        isComplete: false
-    };
-    addTodoToDom(newTodo);
-    todoValue.value = "";
-    todoValue.focus();
+let todos: Todo[] = JSON.parse(localStorage.getItem("todos") || "[]");
+
+const handleSubmit = (event: Event) => {
+  event.preventDefault();
+
+  const newTodo: Todo = {
+    id: crypto.randomUUID(),
+    title: todoValue.value,
+    isComplete: false,
+  };
+
+  addTodoToDom(newTodo);
+  todos.push(newTodo);
+  saveTodosInLocalStorage();
+
+  todoValue.value = "";
+  todoValue.focus();
 };
 
 const addTodoToDom = (todo: Todo) => {
-    todoList.insertAdjacentHTML('beforeend',
-        `
-        <li>
-        ${todo.title}<span class="icon"">
-          <i class="fas fa-trash"></i>
-          </span>
+  todoList.insertAdjacentHTML(
+    "beforeend",
+    `
+        <li onclick="removeTodo('${todo.id}')">
+          ${todo.title}<span class="icon"
+            ><i class="fas fa-trash"></i
+          ></span>
         </li>
-    `)
-}
+    `
+  );
+};
 
-addTodo.addEventListener('click', (event) => nandleSubmit(event))
+const saveTodosInLocalStorage = () => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+  return true;
+};
+
+const removeTodo = (todoID: string) => {
+  todos = todos.filter((todo) => todo.id !== todoID);
+  saveTodosInLocalStorage();
+  todoList.innerHTML = "";
+  todos.forEach((todo) => addTodoToDom(todo));
+};
+
+addTodo.addEventListener("click", (event) => handleSubmit(event));
+
+window.addEventListener("DOMContentLoaded", () =>
+  todos.forEach((todo) => addTodoToDom(todo))
+);
